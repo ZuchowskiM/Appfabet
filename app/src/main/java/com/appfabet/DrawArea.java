1a902d039739991e59708d7c27c0e9075c11a9a1
ZuchowskiM <zimos2449@gmail.com>
2021-03-24 17:32:16 +0100

    Added basic drawing class

---
+++ app/src/main/java/com/appfabet/DrawArea.java
@@ -0,0 +1,68 @@
+package com.appfabet;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DrawArea extends View
+{
+    private final int paintColor = Color.BLACK;
+    private Paint drawPaint;
+    private List<Point> circlePoints;
+    private Path path = new Path();
+
+    public DrawArea(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        setupPaint();
+        circlePoints = new ArrayList<Point>();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawPath(path, drawPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float pointX = event.getX();
+        float pointY = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+
+                path.moveTo(pointX, pointY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+
+                path.lineTo(pointX, pointY);
+                break;
+            default:
+                return false;
+        }
+
+        postInvalidate(); // Indicate view should be redrawn
+        return true;
+    }
+
+    private void setupPaint() {
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(5);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+        drawPaint.setStyle(Paint.Style.STROKE);
+    }
+
+
+}
--- app/src/main/java/com/appfabet/MainActivity.java
+++ app/src/main/java/com/appfabet/MainActivity.java
@@ -2,7 +2,17 @@ package com.appfabet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DrawArea drawArea = findViewById(R.id.drawing);
+
     }
+
+
 }
+
